a2a271342466cf0119c583a22e0cad40b3ad50ca
Srimped <103495935+Srimped@users.noreply.github.com>
2023-02-25 00:39:53 +0700

    sửa button

--- resources/views/Goodi/Account/editAcc.blade.php
+++ resources/views/Goodi/Account/editAcc.blade.php
@@ -52,9 +52,7 @@
             </select>
         </div>
         <button type="submit" class="btn btn-success">Submit</button>
-        <a href="/admin/acc">
-            <button class="btn btn-danger">Back</button>
-        </a>
+        <a class="btn btn-danger" href="/admin/acc">Back</a>
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
