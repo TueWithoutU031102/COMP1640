00c1e9353354bae151bb2c6452e901beb853b728
Srimped <103495935+Srimped@users.noreply.github.com>
2023-02-25 00:14:35 +0700

    sửa navbar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
     }
     public function logout()
     {
-        Auth::guard('admin')->logout();
+        Auth::logout();
         return redirect('/');
     }
 }
--- resources/views/Goodi/nav_bar.blade.php
+++ resources/views/Goodi/nav_bar.blade.php
@@ -8,54 +8,67 @@
             <li><a href="/about" >ABOUT</a></li>
             <li><a href="/department">DEPARTMENT</a></li>
             <li><a href="/FAQ">FAQs</a></li>
-                <li><a href="/login">LOGIN</a></li>
-            @else
-                <li><a href="/">{{Auth::user()->name}}</a></li>
-            @endif
+            <li><a href="/login">LOGIN</a></li>
         </ul>
     </div>
 </nav>
-
-
-<div class="black-nav-link" id="navlink">
-    <ul>
-        <li><a href="/forum" >FORUM</a></li>
-        <li>
-            <div class="action">
-                <div class="profile" onclick="menuToggle();">
-                <a style="cursor: default">MENU</a>
-                </div>
-                <div class="menu">
-                    <ul>
-                        <li><a href="/admin/submission/index">Submissions</a></li>
-                        <li><a href="/admin/acc">Accounts</a></li>
-                        <li><a href="/idea">Ideas</a></li>
-                    </ul>
-                </div>
-            </div>
-        </li>
-        <li><a href="/about" >ABOUT</a></li>
-        <li><a href="/department">DEPARTMENT</a></li>
-        <li>
-            <div class="action">
-                <div class="profile" onclick="profileToggle();">
-                    <img src="images/avatar_63f826aa19394.jpg" alt="mdo" width="50" height="50" class="rounded-circle">
-                </div>
-                <div class="menu_pro">
-                    <div class="info">
-                        <br>
-                        <h3>{{Auth::user()->name}}</h3>
-                        <p>{{Auth::user()->role->name}}</p>
+@else
+<nav>
+    <a href="/" ><img class="logo" src="/css/images/logo_transparent.png"></a>
+    <div class="black-nav-link" id="navlink">
+        <ul>
+            <li><a href="/forum" >FORUM</a></li>
+            <li>
+                <div class="action">
+                    <div class="profile" onclick="menuToggle();">
+                        <a style="cursor: default">MENU</a>
                     </div>
-                    <ul>
-                        <li><a href="#">My Profile</a></li>
-                        <li><a href="#">Edit Profile</a></li>
-                        <li><a href="#">Setting</a></li>
-                        <li><a href="{{ route('logout') }}">Logout</a></li>
-                    </ul>
+                    <div class="menu">
+                        <ul>
+                            <li><a href="/admin/submission/index">Submissions</a></li>
+                            <li><a href="/admin/acc">Accounts</a></li>
+                            <li><a href="/idea">Ideas</a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </li>
-        <li><a href="/FAQ">FAQs</a></li>
-    </ul>
-</div>
+            </li>
+            <li><a href="/about" >ABOUT</a></li>
+            <li><a href="/department">DEPARTMENT</a></li>
+            <li>
+                <div class="action">
+                    <div class="profile" onclick="profileToggle();">
+                        <img src="images/avatar_63f8b0069c831.jpg" alt="mdo" width="50" height="50" class="rounded-circle">
+                    </div>
+                    <div class="menu_pro">
+                        <div class="info">
+                            <br>
+                            <h3>{{Auth::user()->name}}</h3>
+                            <p>{{Auth::user()->role->name}}</p>
+                        </div>
+                        <ul>
+                            <li><a href="#">My Profile</a></li>
+                            <li><a href="#">Edit Profile</a></li>
+                            <li><a href="#">Setting</a></li>
+                            <li><a href="{{ route('logout') }}">Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            <li><a href="/FAQ">FAQs</a></li>
+        </ul>
+    </div>
+</nav>
+@endif
+
+<script>
+    function menuToggle(){
+        const toggleMenu = document.querySelector('.menu');
+        toggleMenu.classList.toggle('active')
+    }
+
+    function profileToggle(){
+        const toggleMenu = document.querySelector('.menu_pro');
+        toggleMenu.classList.toggle('active')
+    }
+</script>
+
