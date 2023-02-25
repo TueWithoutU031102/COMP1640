33317f71849216e1e80c14360b671bfa83a72ab1
Srimped <103495935+Srimped@users.noreply.github.com>
2023-02-26 02:06:25 +0700

    idea front-end

--- resources/views/Goodi/Idea/index.blade.php
+++ resources/views/Goodi/Idea/index.blade.php
@@ -1,9 +1,9 @@
 @extends('Goodi.nav_bar')
 
 @section('main')
-<section class="banner">
+{{-- <section class="banner">
 <br><br><br><br><br><br><br><br><br><br><br><br><br>
-</section><br>
+</section><br> --}}
 <section class="main_idea">
     <div class="idea-container">
         <div class="left-side">
@@ -34,26 +34,47 @@
                     </div>
                 </form>
                 <div class="btn-idea">
-                    <button class="add-idea">+</button>
+                    <button class="add-idea" onclick="formToggle();">+</button>
                     <button class="refresh-idea">Refresh</button>
                 </div>
             </section>
             <section class="create-idea">
                 <h2>New Idea</h2>
+                <i></i>
                 <form action="">
-                    <div class="form-group">
+                    <div class="">
                         <label for="title" class="font-weight-bold">Title</label>
                         <input type="title" name="title" class="form-control" id="title" aria-describedby="title">
                     </div>
-                    <div class="form-group">
+                    <div class="">
                         <label for="discussion" class="font-weight-bold">Discussion</label>
                         <textarea type="discussion" name="discusstion" class="form-control" id="discussion" aria-describedby="discussion" rows="7"></textarea>
+                    </div><br>
+                    <div class="button-idea">
+                        <button class="btn btn-success" style="padding: 10px 100px;" type="submit">Submit</button>
                     </div>
-                    <button type="submit">Submit</button>
-                    <a href="/admin/acc">
-                        <button class="btn btn-danger">Back</button>
-                    </a>
                 </form>
+            </section>
+            <section class="post">
+                <br>
+                <div class="post-container">
+                    <div class="user-detail">
+                        <img src="https://github.com/mdo.png" width="50" height="50"
+                        class="rounded-circle" alt="">
+                        <div class="post-content">
+                            <h4>We need a better solution for teaching children in math<h6>
+                            <small>LongNT Has Posted on Feburary 12, 2023</small><br><br>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.
+                                Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor.
+                                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum,
+                                nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>
+                        </div>
+                    </div>
+                    <div class="idea-interact">
+
+                    </div>
+                </div>
+
             </section>
         </div>
         <div class="right-side">
@@ -63,4 +84,13 @@
 </section>
 
 @endsection
+
+<script>
+    function formToggle() {
+        const toggleForm = document.querySelector('.create-idea');
+        const toggleButton = document.querySelector('.button-idea');
+        toggleForm.classList.toggle('active')
+        toggleButton.classList.toggle('active')
+    }
+</script>
 
