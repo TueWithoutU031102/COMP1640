68f6e2818f1289cf1ca34db57e7e30daaf2e9f27
Srimped <103495935+Srimped@users.noreply.github.com>
2023-02-25 23:53:47 +0700

    Login Form

--- resources/views/Goodi/login.blade.php
+++ resources/views/Goodi/login.blade.php
@@ -1,44 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div class="container_login">
-        <img class="logo" src="images/331294928_2804183029713156_7463841755418801747_n.png" alt="">
-        <h1 class="label"> LOGIN </h1>
-        @if ($errors->any())
+@extends('Master.Master')
 
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <form class="login_form"  method="POST" action="/login">
-            @csrf
-            <div class="font">Email</div>
-            <input type="text" name="email" placeholder="Email">
-            <div class="font font2">Password</div>
-            <input type="password" name="password" placeholder="Password">
-            <br>
-            <div class="check">
-                <input type="checkbox" name="check"> <label for="check">Remember me</label>
-            </div>
-            <button type="submit">Submit</button>
-            <h5>Or Sign-up Using</h5>
-            <div class="logosocial">
-            <a href=""><img src="css/images/—Pngtree—facebook social media icon_8704814.png" padding="100px" alt="" ></a>
-            <a href=""><img src="css/images/Twitter-Logo-PNG-2012-–-Now-2.png" alt=""></a>
-            <a href=""><img  src="css/images/Google__G__Logo.svg.png" alt=""></a>
-            </div>
-        </form>
+@section('main')
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: 0;
+        }
+    </style>
+    <div class="login">
+
+        <div class="login_box">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form class="login_form"  method="POST" action="/login">
+                <h2>Login</h2>
+                <div class="input_box">
+                    <input type="text" name="email" required="required">
+                    <span>Email</span>
+                    <i></i>
+                </div>
+                <div class="input_box">
+                    <input type="password" name="password" required="required">
+                    <span>Password</span>
+                    <i></i>
+                </div>
+                <br>
+                <div class="check">
+                    <input type="checkbox" name="check"> <label for="check">Remember me</label>
+                </div>
+                <button type="submit">Submit</button>
+                <h5>Or Sign-up Using</h5>
+                <div class="logosocial">
+                {{-- <a href=""><img src="css/images/—Pngtree—facebook social media icon_8704814.png" padding="100px" alt="" ></a>
+                <a href=""><img src="css/images/Twitter-Logo-PNG-2012-–-Now-2.png" alt=""></a>
+                <a href=""><img  src="css/images/Google__G__Logo.svg.png" alt=""></a> --}}
+                </div>
+            </form>
+        </div>
     </div>
-</body>
-</html>
+@endsection
