596f0867bb6b09eefd34873af312d62da07c85f2
Srimped <103495935+Srimped@users.noreply.github.com>
2023-04-17 02:48:46 +0700

    forbidden

--- resources/views/403.blade.php
+++ resources/views/403.blade.php
@@ -1,6 +1,13 @@
 @extends('Master.Master')
 
 @section('main')
-    <h1>ban khong co quyen!</h1>
-    <a href="/index">Ve trang chinh</a>
+    <div id="preloader"></div>
+    <div class="forbidden">
+        <h1 class="for-highlight">Oops!</h1>
+        <h1 class="for-box">403</h1>
+        <a href="/">
+            <h2>&rightarrow; BACK TO HOME &LeftArrow;</h2>
+        </a>
+        <p>YOU DO NOT HAVE PERMISSION TO ACCESS THE DOCUMENT OR PROGRAM THAT YOU REQUESTED.</p>
+    </div>
 @endsection
