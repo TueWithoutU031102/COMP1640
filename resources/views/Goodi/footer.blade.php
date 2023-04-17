42a30b550da473cf862fb1cf587cc79384d8877e
Srimped <103495935+Srimped@users.noreply.github.com>
2023-04-18 02:27:01 +0700

    sua footer

--- resources/views/Goodi/footer.blade.php
+++ resources/views/Goodi/footer.blade.php
@@ -1,16 +1,33 @@
 <footer>
     <div class="footer-content">
-        <h3></h3>
-        <p></p>
+        <h3>MORE ABOUT COMPANY</h3>
+        <p>Goodi is a project to construct a website for the personnel of a university. Site administrators can post
+            contributions at this site to solicit staff suggestions for improving the university environment. Not only
+            that, the website is also integrated with a forum for school personnel to share and support one other in the
+            process of working</p>
+            <br>
+        <h3>CONTACT INFOMATION</h3>
+        <p><i class="fa fa-globe" aria-hidden="true"></i> GOODI WEBSITE</p>
+        <p><i class="fa fa-home" aria-hidden="true"></i> Số 2 Phạm Văn Bạch, Cầu Giấy, Hà Nội</p>
+        <p><i class="fa fa-phone" aria-hidden="true"></i> 0369782002</p>
+        <p><i class="fa fa-envelope" aria-hidden="true"></i> goodiwebsite@gmail.com</p>
+        <br>
         <ul class="socials">
-            <li><a href="https://www.instagram.com/glvssbrvnd/" class="contact-btn"><i class="fab fa-instagram"></i></a></li>
-            <li><a href="https://www.facebook.com/glvssbrvnd/" class="contact-btn"><i class="fab fa-facebook-square"></i></a></li>
-            <li><a href="https://twitter.com/glvssbrvnd" class="contact-btn"><i class="fab fa-twitter-square"></i></a></li>
-            <li><a href="https://www.pinterest.com/glvssbrvnd/_saved/" class="contact-btn"><i class="fab fa-pinterest-square"></i></a></li>
+            <li><a href="https://www.instagram.com/goodiwebsite/?fbclid=IwAR36gRPFJqMbs8cpfMezNLA2qt0ryg7osZFHCXL60JAUxWiQCbFWl-h0vkU"
+                    class="contact-btn"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://www.facebook.com/profile.php?id=100091826745211" class="contact-btn"><i
+                        class="fab fa-facebook-square"></i></a></li>
+            <li><a href="https://twitter.com/WGoodi?fbclid=IwAR1Wy0ZJE7qmh0G9qlnCsXy0bjjIAzMwsT83bDeB2GXf74RKzZTMFeocvc0"
+                    class="contact-btn"><i class="fab fa-twitter-square"></i></a>
+            </li>
+            <li><a href="https://www.pinterest.com/goodiwebsite/?fbclid=IwAR0NAKgCnzowGpKnbHJVKi1nWRpQsSLMWDj9SW_sLVsHrX0XIv-Aj9EkcMs"
+                    class="contact-btn"><i class="fab fa-pinterest-square"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/website-goodi-521a95272/?fbclid=IwAR1uUw5NMJUcl8SSktlNmHrhRPBdHK1jznZQdW84HJkgzGYEBeK8la8KBIs"
+                    class="contact-btn"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
         </ul>
     </div>
     <div class="footer-bottom">
-        <p><strong>Wish to see you in the future</strong></p>
+        <p><strong>© 2023 Goodi, All Right Reserved | The Goodi Company
+            </strong></p>
     </div>
 </footer>
-
