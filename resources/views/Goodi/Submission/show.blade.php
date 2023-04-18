87cc46fa84640ddcfe18c26f35f2eacc8dfd8c3c
Ku Tue <vunguyenductue@gmail.com>
2023-04-18 13:51:41 +0700

    Revert "xóa submission"
    This reverts commit 3e7654ac7f17bb12187260f935327ba385cf6337.

--- resources/views/Goodi/Submission/show.blade.php
+++ resources/views/Goodi/Submission/show.blade.php
@@ -17,7 +17,7 @@
                 overflow: hidden;
             }
 
-            #view{{ $idea->id }} {
+            #view{{ $idea->id }}    {
                 display: none;
             }
 
@@ -29,21 +29,21 @@
             }
 
 
-            #view{{ $idea->id }}:checked~.des {
+            #view{{ $idea->id }}:checked ~ .des {
                 --max-line: 0;
             }
 
-            #view{{ $idea->id }}:checked~label {
+            #view{{ $idea->id }}:checked ~ label {
                 visibility: hidden;
             }
 
-            #view{{ $idea->id }}:checked~label:after {
+            #view{{ $idea->id }}:checked ~ label:after {
                 content: 'Show Less';
                 display: block;
                 visibility: visible;
             }
 
-            .gradient-custom{{ $idea->id }} {
+            .gradient-custom{{ $idea->id }}    {
                 height: 0;
                 display: none;
                 /* transition: 0.2s; */
@@ -54,7 +54,7 @@
                 display: block;
             }
 
-            .idea-effect{{ $idea->id }} {
+            .idea-effect{{ $idea->id }}    {
                 display: none;
                 letter-spacing: 0;
                 position: absolute;
@@ -70,7 +70,7 @@
             }
 
 
-            .idea-change{{ $idea->id }} {
+            .idea-change{{ $idea->id }}    {
                 position: absolute;
                 right: 30px;
                 background: #fff;
@@ -107,7 +107,7 @@
                 background: #8f8f8f;
             }
 
-            @media (width <=800px) {
+            @media (width <= 800px) {
                 .des {
                     width: auto;
                     --max-line: 3;
@@ -141,7 +141,7 @@
                 <div class="left-side">
                     <div class="profile-display">
                         <img src="{{ asset(Auth::user()->image) }}" alt="mdo" width="50" height="50"
-                            class="rounded-circle" style="object-fit: cover; object-position: center center;">
+                             class="rounded-circle" style="object-fit: cover; object-position: center center;">
                         <h5 style="font-weight: bold">{{ Auth::user()->name }}</h5>
                     </div>
                     <div class="imp-link">
@@ -179,10 +179,10 @@
                                     onclick="getTimeRemaining('{{ $submission->startDate }}', '{{ $submission->dueDate }}', this)">
                                     ||</p> --}}
                                 <p onclick="getTimeRemaining('{{ $submission->dueDate }}', this)"
-                                    @if ($isDue) style="color: red" @endif>{{ $timeRemaining }}</p>
+                                   @if ($isDue) style="color: red" @endif>{{ $timeRemaining }}</p>
                                 <p><span>Description: </span>{{ $submission->title }}</p>
                                 <form action="{{ $submission->id }}" method="POST" class="d-inline"
-                                    onsubmit="return confirm('Are you sure to delete {{ $submission->title }} !!!???')">
+                                      onsubmit="return confirm('Are you sure to delete {{ $submission->title }} !!!???')">
                                     @csrf
                                     {{-- <button class="btn btn-danger"><i aria-hidden="true">Delete</i></button> --}}
                                 </form>
@@ -247,18 +247,18 @@
                             <div class="form-group">
                                 <label for="dueDate" class="font-weight-bold">Due Date:</label>
                                 <input type="title" name="dueDate" class="form-control" id="dueDate"
-                                    aria-describedby="title" value="{{ $submission->dueDate }}" disabled>
+                                       aria-describedby="title" value="{{ $submission->dueDate }}" disabled>
                             </div>
                             <div class="form-group">
                                 <label for="title" class="font-weight-bold">Title:</label>
                                 <input type="title" name="title" class="form-control" id="title"
-                                    aria-describedby="title">
+                                       aria-describedby="title">
                             </div>
                             <div class="form-group">
                                 <label for="category_id" class="font-weight-bold">Category:</label>
 
                                 <select name="category_id" value="{{ old('category_id') }}" class="form-select"
-                                    id="category" aria-label="Category">
+                                        id="category" aria-label="Category">
                                     @foreach ($listCategories as $category)
                                         <option value="{{ $category->id }}">{{ $category->title }}</option>
                                     @endforeach
@@ -266,8 +266,9 @@
                             </div>
                             <div class="form-group">
                                 <label for="description" class="font-weight-bold">Description: </label>
-                                <textarea type="description" name="description" class="form-control" id="discussion" aria-describedby="description"
-                                    rows="4"></textarea>
+                                <textarea type="description" name="description" class="form-control" id="discussion"
+                                          aria-describedby="description"
+                                          rows="4"></textarea>
                             </div>
                             <br>
                             <div class="form-group">
@@ -287,7 +288,7 @@
                             <div class="post-container">
                                 <div class="change">
                                     <button class="idea-change{{ $idea->id }}"
-                                        onclick="ideaToggle({{ $idea->id }});">
+                                            onclick="ideaToggle({{ $idea->id }});">
                                         <p>&dot;&dot;&dot;</p>
                                     </button>
                                     <div class="idea-effect{{ $idea->id }}">
@@ -297,16 +298,15 @@
                                             @if ($idea->author_id == Auth::user()->id || Auth::user()->id == 1)
                                                 <li>
                                                     <a onclick="return confirm('Are {{ $idea->user->name }} sure to delete {{ $idea->title }} !!!???')"
-                                                        href="/idea/delete/{{ $idea->id }}">Remove Post</a>
-                                                </li>
+                                                       href="/idea/delete/{{ $idea->id }}">Remove Post</a></li>
                                             @endif
                                         </ul>
                                     </div>
                                 </div>
                                 <div class="user-detail">
                                     <img src="{{ asset($idea->user->image) }}" width="50" height="50"
-                                        class="rounded-circle" alt=""
-                                        style="object-fit: cover; object-position: center center;">
+                                         class="rounded-circle" alt=""
+                                         style="object-fit: cover; object-position: center center;">
                                     <div class="post-content">
                                         <h4>{{ $idea->title }}</h4>
                                         </a>
@@ -327,15 +327,15 @@
                                     <div>
                                         @if (!$idea->likedBy(auth()->user()))
                                             <button type="submit"
-                                                onclick="likeIdea({{ $idea->id }}, 'likesCount{{ $idea->id }}', 'dislikesCount{{ $idea->id }}')">
+                                                    onclick="likeIdea({{ $idea->id }}, 'likesCount{{ $idea->id }}', 'dislikesCount{{ $idea->id }}')">
                                                 <i class="fa-regular fa-thumbs-up fa-2x"
-                                                    id="likes-interact{{ $idea->id }}"></i>
+                                                   id="likes-interact{{ $idea->id }}"></i>
                                             </button>
                                         @else
                                             <button type="submit"
-                                                onclick="likeIdea({{ $idea->id }}, 'likesCount{{ $idea->id }}', 'dislikesCount{{ $idea->id }}')">
+                                                    onclick="likeIdea({{ $idea->id }}, 'likesCount{{ $idea->id }}', 'dislikesCount{{ $idea->id }}')">
                                                 <i class="fa-solid fa-thumbs-up fa-2x"
-                                                    id="likes-interact{{ $idea->id }}"></i>
+                                                   id="likes-interact{{ $idea->id }}"></i>
                                             </button>
                                         @endif
                                         <h6 id="likesCount{{ $idea->id }}">{{ $idea->likes->count() }}</h6>
@@ -343,15 +343,15 @@
                                     <div>
                                         @if (!$idea->dislikedBy(auth()->user()))
                                             <button type="submit"
-                                                onclick="dislikeIdea({{ $idea->id }}, 'likesCount{{ $idea->id }}', 'dislikesCount{{ $idea->id }}')">
+                                                    onclick="dislikeIdea({{ $idea->id }}, 'likesCount{{ $idea->id }}', 'dislikesCount{{ $idea->id }}')">
                                                 <i class="fa-regular fa-thumbs-down fa-2x"
-                                                    id="dislikes-interact{{ $idea->id }}"></i>
+                                                   id="dislikes-interact{{ $idea->id }}"></i>
                                             </button>
                                         @else
                                             <button type="submit"
-                                                onclick="dislikeIdea({{ $idea->id }}, 'likesCount{{ $idea->id }}', 'dislikesCount{{ $idea->id }}')">
+                                                    onclick="dislikeIdea({{ $idea->id }}, 'likesCount{{ $idea->id }}', 'dislikesCount{{ $idea->id }}')">
                                                 <i class="fa-solid fa-thumbs-down fa-2x"
-                                                    id="dislikes-interact{{ $idea->id }}"></i>
+                                                   id="dislikes-interact{{ $idea->id }}"></i>
                                             </button>
                                         @endif
                                         <h6 id="dislikesCount{{ $idea->id }}">{{ $idea->dislikes->count() }}</h6>
@@ -368,7 +368,7 @@
                                     <div>
                                         <button>
                                             <a style="text-decoration: none; color:#000"
-                                                href="/idea/show/{{ $idea->id }}"><i
+                                               href="/idea/show/{{ $idea->id }}"><i
                                                     class="fa-solid fa-eye fa-2x"></i></a>
                                         </button>
                                         <h6>{{ $idea->views }}</h6>
@@ -393,29 +393,26 @@
                             <p><span>Due date 2: </span><i id="dueDate2Info">{{ $submission->dueDateComment }}</i></p>
                             <span>Time remaining: </span>
                             <p onclick="getTimeRemaining('{{ $submission->dueDate }}', this)"
-                                @if ($isDue) style="color: red" @endif>{{ $timeRemaining }}</p>
+                               @if ($isDue) style="color: red" @endif>{{ $timeRemaining }}</p>
                             <p><span>Description: </span>{{ $submission->title }}</p>
                             <button class="btn btn-primary"
-                                onclick="editDate('{{ $submission->dueDate }}', '{{ $submission->dueDateComment }}')"
-                                id="editButton">
+                                    onclick="editDate('{{ $submission->dueDate }}', '{{ $submission->dueDateComment }}')"
+                                    id="editButton"
+                            >
                                 <i aria-hidden="true"><i class="fa-solid fa-pen"></i> </i>
                             </button>
                             <input type="text" id="editStatus" value="0" hidden>
-                            <input type="datetime-local" value="{{ $submission->startDate }}" id="editStartDateInput"
-                                hidden>
+                            <input type="datetime-local" value="{{$submission->startDate}}" id="editStartDateInput"
+                                   hidden>
                             <button class="btn btn-primary" id="submitEditDate" hidden
-                                onclick="updateDate('{{ $submission->id }}')">
+                                    onclick="updateDate('{{$submission->id}}')">
                                 Ok
                             </button>
-                            <button class="btn btn-danger">
-                                <a onclick="return confirm('Are you sure to delete {{ $submission->title }} !!!???')"
-                                    href="/submission/delete/{{ $submission->id }}">
-                                    <i aria-hidden="true">
-                                        <i class="fa-solid fa-trash">
-                                        </i>
-                                    </i>
-                                </a>
-                            </button>
+                            <form action="{{ $submission->id }}" method="POST" class="d-inline"
+                                  onsubmit="return confirm('Are you sure to delete {{ $submission->title }} !!!???')">
+                                @csrf
+                                {{-- <button class="btn btn-danger"><i aria-hidden="true">Delete</i></button> --}}
+                            </form>
                         </div>
                     </div>
                     {{--                          edit date form --}}
@@ -469,17 +466,18 @@
                 }
 
             }
-
             function updateDate(submissionId) {
                 let dueDateInput = document.getElementById('editDueDateInput').value;
                 let dueDate2Input = document.getElementById('editDueDate2Input').value;
 
-                let url =
-                    `{{ route('updateSubmission', [
-                        'id' => '_submissionId',
-                        'dueDate' => '_newDate',
-                        'dueDateComment' => '_newDate2',
-                    ]) }}`;
+                let url = `{{ route('updateSubmission',
+                                    [
+                                        'id' => '_submissionId',
+                                        'dueDate' => '_newDate',
+                                        'dueDateComment' => '_newDate2'
+                                    ]
+                                   )
+                           }}`;
                 url = url.replace('_submissionId', submissionId)
                 url = url.replace('_newDate', dueDateInput)
                 url = url.replace('_newDate2', dueDate2Input)
@@ -515,7 +513,7 @@
                 } else {
                     button.disabled = false;
                 }
-                if (dueDate1Input.value == '' || dueDate2Input.value == '') {
+                if (dueDate1Input.value == '' || dueDate2Input.value == ''){
                     button.disabled = true;
                 }
             }
